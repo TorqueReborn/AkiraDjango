820b033b7a967d59a5df68b079a0cc77e924ab57
TorqueReborn <torquereborn@gmail.com>
2026-01-03 12:48:38 +0530

    frontend: Test test api route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,14 @@
+import { useEffect } from "react"
+
 const App = () => {
+  useEffect(() => {
+    const getResponse = async () => {
+      const response = await fetch('http://127.0.0.1:8000/test/')
+      console.log(response)
+    }
+    getResponse()
+  }, [])
+
   return (
     <div className="bg-black text-white">App</div>
   )
