59bde7831959674105212424af9b9758b2f4055c
TorqueReborn <torquereborn@gmail.com>
2026-01-03 11:40:11 +0530

    backend: Authenticate user based on username and password

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,9 +1,20 @@
 from rest_framework import status
 from rest_framework.views import APIView
-from django.contrib.auth import authenticate
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+
+from django.contrib.auth import authenticate
 
 class LoginAPIView(APIView):
     def post(self, request):
-        return Response({"message": "This is nice"})
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if not user:
+            return Response(
+                {"error": "Invalid credentials"},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+        
+        return Response({"message": "Authenticated successfully"})
