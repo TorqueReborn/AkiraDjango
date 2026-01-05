371acb7c7b630c7779b1a84644bf98fc86625822
TorqueReborn <torquereborn@gmail.com>
2026-01-05 09:56:17 +0530

    backend: set TokenBackend as authentication backend

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -86,6 +86,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'project.wsgi.application'
 
+AUTHENTICATION_BACKENDS = [
+    'accounts.auth_backends.TokenBackend',
+]
+
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
