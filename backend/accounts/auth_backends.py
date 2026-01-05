4baf745c39b92b599e063372930d5e2b8bb9625a
TorqueReborn <torquereborn@gmail.com>
2026-01-05 16:34:52 +0530

    backend: If token is received, check token and then user and authenticate

--- backend/accounts/auth_backends.py
+++ backend/accounts/auth_backends.py
@@ -1,15 +1,23 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
 
+from .models import Token
+
 User = get_user_model()
 
 class TokenBackend(ModelBackend):
     def authenticate(self, request, username = None, password=None, token = None, **kwargs):
-        if token and username:
+        if token:
             try:
-                user = User.objects.get(username=username, token=token)
-                return user
-            except User.DoesNotExist:
+                token_obj = Token.objects.select_related('user').get(token=token)
+            except Token.DoesNotExist:
                 return None
+            
+            user = token_obj.user
+
+            if username and user.username != username:
+                return None
+            
+            return user
         
         return super().authenticate(request, username=username, password=password, **kwargs)
