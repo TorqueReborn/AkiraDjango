aa379fcf8354886512d2cf2460db01eefaff0411
TorqueReborn <torquereborn@gmail.com>
2026-01-05 16:14:57 +0530

    backend; Drop avatar and token fields

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -5,17 +5,6 @@ from django.contrib.auth.models import AbstractUser
 from rest_framework_simplejwt.tokens import RefreshToken
 
 class User(AbstractUser):
-    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
-    token = models.TextField(blank=True)
-
-    def save(self, *args, **kwargs):
-        is_new = self.pk is None
-        super().save(*args, **kwargs)
-
-        if is_new and not self.token:
-            refresh = RefreshToken.for_user(self)
-            self.token = str(refresh)
-            super().save(update_fields=['token'])
 
     def __str__(self):
         return self.username
