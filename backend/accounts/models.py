974914e774136e06a91961d6068a74d3271cd6f4
TorqueReborn <torquereborn@gmail.com>
2026-01-05 17:29:39 +0530

    backend: Create token using uuid to copt with conflict

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -6,8 +7,9 @@ class User(AbstractUser):
         return self.username
 
 class Token(models.Model):
-    token = models.TextField(blank=True, db_index=True)
+    token = models.TextField(default=uuid.uuid4, db_index=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tokens')
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.token
