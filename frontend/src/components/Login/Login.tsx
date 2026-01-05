a6bf61d9dc81bac5b6c6069fb630691a8ce3b060
TorqueReborn <torquereborn@gmail.com>
2026-01-05 17:19:12 +0530

    frontend: Stylize Login Screen

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -12,65 +12,52 @@ const Login = () => {
         setFormData((prev) => ({ ...prev, [name]: value }));
     };
 
-    const handleClick = async (e: React.FormEvent) => {
-        e.preventDefault();
-        let response = await fetch('http://127.0.0.1:8000/api/refresh/', {
-            method: "POST",
-            credentials: "include",
-        });
-
-        if (!response.ok) {
-            throw new Error("Login failed");
-        }
-
-        console.log(await response.json());
-    }
-
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
         await fetch('http://127.0.0.1:8000/api/login/', {
             method: "POST",
             headers: {
-            "Content-Type": "application/json",
+                "Content-Type": "application/json",
             },
             body: JSON.stringify({
-            username: formData.username,
-            password: formData.password,
+                username: formData.username,
+                password: formData.password,
             }),
             credentials: "include",
         }).then(response => response.json())
-        .then(data => {
-            const accessToken = data.access_token;
-            localStorage.setItem('access_token', accessToken);
-        });;
+            .then(data => {
+                const accessToken = data.access_token;
+                localStorage.setItem('access_token', accessToken);
+            });;
     }
 
     return (
-    <form className="login-card" onSubmit={handleSubmit}>
-    <h2>Login</h2>
+        <div className="flex items-center justify-center h-screen">
+            <form className="bg-slate-600 flex flex-col" onSubmit={handleSubmit}>
+                <h2>Login</h2>
 
-    <input
-        type="username"
-        name="username"
-        placeholder="Username"
-        value={formData.username}
-        onChange={handleChange}
-        required
-    />
+                <input
+                    type="username"
+                    name="username"
+                    placeholder="Username"
+                    value={formData.username}
+                    onChange={handleChange}
+                    required
+                />
 
-    <input
-        type="password"
-        name="password"
-        placeholder="Password"
-        value={formData.password}
-        onChange={handleChange}
-        required
-    />
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    value={formData.password}
+                    onChange={handleChange}
+                    required
+                />
 
-    <button type="submit">Login</button>
-    <button onClick={handleClick}>Refresh</button>
-    </form>
-  );
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
 }
 
 
