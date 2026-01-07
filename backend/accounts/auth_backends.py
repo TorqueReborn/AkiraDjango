c6fee93477d406149c91f861ad54d77590751efa
TorqueReborn <torquereborn@gmail.com>
2026-01-07 10:56:44 +0530

    backend: Fix auth_backend to take in username too * previously it was unused

--- backend/accounts/auth_backends.py
+++ backend/accounts/auth_backends.py
@@ -9,11 +9,8 @@ class TokenBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, token=None, **kwargs):
         if token:
             try:
-                token_obj = Token.objects.select_related('user').get(token=token)
+                token_obj = Token.objects.get(user__username=username, token=token)
             except Token.DoesNotExist:
                 return None
-            user = token_obj.user
-            if username and user.username != username:
-                return None
-            return user
+            return token_obj.user
         return super().authenticate(request, username=username, password=password, **kwargs)
