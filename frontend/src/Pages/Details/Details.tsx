fe02924ccae9c917137edd3217e0f91e7d4b9a27
TorqueReborn <torquereborn@gmail.com>
2026-01-07 16:56:54 +0530

    frontend: Show anime details on details page

--- frontend/src/Pages/Details/Details.tsx
+++ frontend/src/Pages/Details/Details.tsx
@@ -1,18 +1,34 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+
+interface AnimeData {
+    id: string,
+    name: string,
+    englishName: string,
+    thumbnail: string,
+    description: string,
+}
 
 const Details = () => {
   const {id} = useParams()
+  const [animeData, setAnimeData] = useState<AnimeData>()
+
   useEffect(() => {
     const getDetails = async () => {
       const response = await fetch(`${import.meta.env.VITE_BACK_END_URL}/details/?id=${id}`)
+      const json = await response.json()
+      setAnimeData(json)
     }
     getDetails()
   }, [])
   
   return (
     <div>
-      
+      {animeData && (<div>
+        <img src={animeData.thumbnail}/>
+        {animeData.englishName} <br/>
+        {animeData.description}
+      </div>)}
     </div>
   )
 }
