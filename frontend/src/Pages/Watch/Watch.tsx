095b26fd7d67ac935fa1d872fdd7f1037b1a3802
TorqueReborn <torquereborn@gmail.com>
2026-01-07 18:42:04 +0530

    frontend: Play episode of anime

--- frontend/src/Pages/Watch/Watch.tsx
+++ frontend/src/Pages/Watch/Watch.tsx
@@ -1,6 +1,26 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+
 const Watch = () => {
+  const {id, episode} = useParams()
+  const [episodeUrl, setEpisodeUrl] = useState('')
+
+  useEffect(() => {
+    const getData = async () => {
+      let response = await fetch(`${import.meta.env.VITE_BACK_END_URL}/watch/?id=${id}`)
+      let json = await response.json()
+      response = await fetch(json[0])
+      json = await response.json()
+      setEpisodeUrl(json.links[0].link)
+    }
+    getData()
+  }, [])
+
+
   return (
-    <div>Watch</div>
+    <div>
+      <video src={episodeUrl} controls={true}></video>
+    </div>
   )
 }
 
