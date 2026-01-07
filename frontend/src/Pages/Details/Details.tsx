2b29247a367df234e43f9ff0d82c3b782ab5687b
TorqueReborn <torquereborn@gmail.com>
2026-01-07 16:43:35 +0530

    frontend: Update Deails component to use it

--- frontend/src/Pages/Details/Details.tsx
+++ frontend/src/Pages/Details/Details.tsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 
 const Details = () => {
+  const {id} = useParams()
+  useEffect(() => {
+    const getDetails = async () => {
+      const response = await fetch(`${import.meta.env.VITE_BACK_END_URL}/details/?id=${id}`)
+    }
+    getDetails()
+  }, [])
+  
   return (
     <div>Details</div>
   )
