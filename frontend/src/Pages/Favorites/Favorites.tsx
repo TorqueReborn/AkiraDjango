597c424a3b74c6d4dccc7e3cb2ff4fbf0ad915e8
TorqueReborn <torquereborn@gmail.com>
2026-01-08 16:25:33 +0530

    frontend: Fetch favorites from backend

--- frontend/src/Pages/Favorites/Favorites.tsx
+++ frontend/src/Pages/Favorites/Favorites.tsx
@@ -1,4 +1,8 @@
 const Favorites = () => {
+  const getFavorites = async () => {
+    const response = await fetch('http://127.0.0.1:8000/favorites/', {credentials: "include"})
+  }
+  getFavorites();
   return (
     <div>Favorites</div>
   )
