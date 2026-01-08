ecf7031412f333e319e29de2102585c123beaf60
TorqueReborn <torquereborn@gmail.com>
2026-01-08 15:36:59 +0530

    frontend: Show saved logins of user

--- frontend/src/Pages/SavedLogins/SavedLogins.tsx
+++ frontend/src/Pages/SavedLogins/SavedLogins.tsx
@@ -1,15 +1,27 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 const SavedLogins = () => {
+  const [data, setData] = useState()
   
   useEffect(() => {
     const testReponse = async () => {
       const response = await fetch('http://127.0.0.1:8000/api/saved_logins/' ,{credentials: "include"})
+      const json = await response.json()
+      setData(json.logins)
     }
     testReponse()
   }, [])
   return (
-    <div>SavedLogins</div>
+    <>
+      <div>SavedLogins</div>
+      { data && (
+        data.map(d => (
+          <div>
+            {d.user} - {d.os} - {d.created_at}
+          </div>
+        ))
+      )}
+    </>
   )
 }
 
