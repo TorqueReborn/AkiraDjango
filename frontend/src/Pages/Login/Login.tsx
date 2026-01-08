db5875a940f3018d57a5c0718c5e160e0f8aa659
TorqueReborn <torquereborn@gmail.com>
2026-01-08 10:17:53 +0530

    frontend: Create Login screen

--- frontend/src/Pages/Login/Login.tsx
+++ frontend/src/Pages/Login/Login.tsx
@@ -1,6 +1,33 @@
-const Login = () => {
+import React, {useState} from "react";
+
+const Login: React.FC = () => {
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleSubmit = async (
+    e: React.FormEvent<HTMLFormElement>
+  ) => {
+    e.preventDefault();
+    const response = await fetch(`${import.meta.env.VITE_BACK_END_URL}/api/login/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username,
+        password
+      })
+    })
+    console.log(await response.text())
+  }
   return (
-    <div>Login</div>
+    <div className="bg-white text-black">
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={username} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setUsername(e.target.value)} placeholder="Username"/>
+        <input type="password" value={password} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)} placeholder="Password"/>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   )
 }
 
