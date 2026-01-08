f3be5e64bf6a71437c1be497111af73b29314067
TorqueReborn <torquereborn@gmail.com>
2026-01-08 09:54:26 +0530

    frontend: Navigate to Login when button clicked

--- frontend/src/Nav/components/Button.tsx
+++ frontend/src/Nav/components/Button.tsx
@@ -1,6 +1,14 @@
+import { useNavigate } from "react-router-dom"
+
 const Button = () => {
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    navigate(`/login`)
+  }
+  
   return (
-    <div className='text-xl text-bold bg-gray-500 px-3 py-1 rounded-2xl hover:bg-amber-50 hover:text-black cursor-pointer'>
+    <div onClick={handleClick} className='text-xl text-bold bg-gray-500 px-3 py-1 rounded-2xl hover:bg-amber-50 hover:text-black cursor-pointer'>
         Login
     </div>
   )
