5d0fb4361c3c4b2696badd3718de610ca60178f9
TorqueReborn <torquereborn@gmail.com>
2026-01-08 17:06:27 +0530

    frontend: Show favorites anime

--- frontend/src/Pages/Favorites/Favorites.tsx
+++ frontend/src/Pages/Favorites/Favorites.tsx
@@ -1,10 +1,26 @@
+import { useEffect, useState } from "react"
+
 const Favorites = () => {
-  const getFavorites = async () => {
-    await fetch('http://127.0.0.1:8000/favorites/', {credentials: "include"})
-  }
-  getFavorites();
+  const [data, setData] = useState()
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch('http://127.0.0.1:8000/favorites/', { credentials: "include" })
+      const json = await response.json()
+      setData(json)
+    }
+    getData()
+  }, [])
+
   return (
-    <div>Favorites</div>
+    <>
+      <div>Favorites</div>
+      {data && data.map(d => (
+        <div>
+          {d.animeName}
+        </div>
+      ))}
+    </>
   )
 }
 
