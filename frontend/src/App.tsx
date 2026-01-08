8a49f739d4643f39a746808d18d58b784c531787
TorqueReborn <torquereborn@gmail.com>
2026-01-08 10:24:38 +0530

    frontend: Check home to see if cookies are there

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
 import Nav from "./Nav/Nav";
+import { useEffect } from "react";
 import Spotlight from "./Pages/Home/Spotlight/Spotlight";
 import Trending from "./Pages/Home/Trending/Trending";
 
 const Login: React.FC = () => {
+    useEffect(() => {
+        const getHome = async () => {
+            const response = await fetch('http://127.0.0.1:8000/api/home/', {credentials: "include"})
+            console.log(await response.text())
+        }
+        getHome();
+    }, [])
+
     return <>
         <Nav/>
         <Spotlight/>
