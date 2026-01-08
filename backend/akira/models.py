3a2a7eca3296f01f17858321d98d1f04ba593e18
TorqueReborn <torquereborn@gmail.com>
2026-01-08 11:06:54 +0530

    backend: Set animeId is unique (It should be)

--- backend/akira/models.py
+++ backend/akira/models.py
@@ -1,8 +1,16 @@
 from django.db import models
+from accounts.models import User
 
 class Anime(models.Model):
-    animeId = models.CharField(max_length=50)
+    animeId = models.CharField(max_length=50, unique=True)
     name = models.CharField(max_length=255)
     
     def __str__(self):
         return self.name
+
+class Favorites(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='favorites')
+    anime = models.ForeignKey(Anime, on_delete=models.CASCADE, related_name='favorites')
+
+    def __str__(self):
+        return self.anime.name
